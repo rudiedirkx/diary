ae7a902880607e0bab1663167ad4c6421520404e
Rudie Dirkx <github@hotblocks.nl>
2022-09-14 02:18:09 +0200

    favicon+

--- tpl.header.php
+++ tpl.header.php
@@ -11,6 +11,9 @@ header('Content-type: text/html; charset=utf-8');
 	<meta name="theme-color" content="#333" />
 	<meta name="referrer" content="no-referrer" />
 	<title>The Diary 2000</title>
+	<link rel="icon" type="image/png" href="/favicon-128.png" sizes="128x128" />
+	<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<style>
 	* {
